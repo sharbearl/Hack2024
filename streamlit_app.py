170a4c99741619b6cac4e51344cefe0e7b09068b
sunadrian <120338538+sunadrian@users.noreply.github.com>
2024-01-27 10:51:38 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,23 @@
 import streamlit as st
 
-col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3, col4, col5 = st.columns(4)
 
 with col1.expander("Home"):
   st.write("this is a test")
 
-# with col2.expander("Water Log"):
+with col2.expander("Water Log"):
   # choice = st.number_input(label = "Adjust water", step = 1)
-  # st.write("this is a test")
-with col2.checkbox("Water log"):
-    choice = st.number_input(label = "Adjust water", step = 1)
-    st.write("this is a test")
+  st.write("this is a test")
   
 with col3.expander("Food Log"):
   st.write("this is a test")
   
 with col4.expander("Exercise Log"):
   st.write("this is a test")
-  
+
+with col4.checkbox("Water check"):
+    choice = st.number_input(label = "Adjust water", step = 1)
+    st.write("this is a test")
 
 # USER INPUT + VARIABLE STORING
 # weight = st.text_input("Enter some text")
