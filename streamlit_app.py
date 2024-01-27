359cffd60c7a9ac02e65fab445b889db2999831c
Sharon <68297755+sharbearl@users.noreply.github.com>
2024-01-27 11:25:35 -0800

    Update streamlit_app.py
    Attempt update

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ with col4.expander("Exercise Log"):
 water_expander = col5.expander("Water log")
 print("TEST PRINT")
 if (water_expander):
-    choice = st.number_input(label = "Adjust water", min_value = 0, step = 1)
+    choice = st.number_input(label = "Adjust water", min_value = 0, step = 1, on_change = update)
     st.write("this is a test")
 
 def update():
