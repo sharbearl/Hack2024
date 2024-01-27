c3eafa122ad86eedf4b8bb7854ab08e328308096
sunadrian <120338538+sunadrian@users.noreply.github.com>
2024-01-27 10:05:03 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # USER INPUT + VARIABLE STORING
 try:
-  double weight = st.text_input('Enter some text')
+  weight = st.text_input('Enter some text')
 except ValueError as e:
   st.text(f”Error: {e}”)
 
