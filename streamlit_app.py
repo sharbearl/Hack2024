de006a75a7c7af47e7ddcccacdcbefa5c95b2ec0
sunadrian <120338538+sunadrian@users.noreply.github.com>
2024-01-27 10:44:35 -0800

    commenting out error parts

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ with col1.expander("Home"):
   st.write("this is a test")
 
 with col2.expander("Water Log"):
-  choice = st.number_input(label = "Adjust water", step = 1)
+  # choice = st.number_input(label = "Adjust water", step = 1)
   st.write("this is a test")
   
 with col3.expander("Food Log"):
