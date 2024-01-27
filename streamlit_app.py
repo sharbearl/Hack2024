e3a9fe7ccb3e3b8c55036f998eedea508d856468
sunadrian <120338538+sunadrian@users.noreply.github.com>
2024-01-27 11:45:40 -0800

    fixing indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,38 +9,38 @@ def update_progress():
 
 if __name__ == "__main__":
 
-col1, col2, col3, col4, col5 = st.columns(5)
-water_val = 0;
-
-water_text = "Water"
-water_bar = st.progress(0, text=water_text)
-
-with col1.expander("Home"):
-  st.write("this is a test")
-
-with col2.expander("Water Log"):
-  # choice = st.number_input(label = "Adjust water", step = 1)
-  st.write("this is a test")
-  
-with col3.expander("Food Log"):
-  st.write("this is a test")
-  
-with col4.expander("Exercise Log"):
-  st.write("this is a test")
-
-# water_checkbox = col5.checkbox("Water check")
-  
-water_expander = col5.expander("Water log")
-if (water_expander):
-    choice = st.number_input(label = "Adjust water", min_value = 0, step = 1, on_change = update)
-    st.write("this is a test")
-
-# USER INPUT + VARIABLE STORING
-# weight = st.text_input("Enter some text")
-form = st.form("Water")
-with st.sidebar:
-with col5.expander(“Adjust Water”):
-	st.text("BOO")
-
-st.image("cup.png")
-return 0
+	col1, col2, col3, col4, col5 = st.columns(5)
+	water_val = 0;
+	
+	water_text = "Water"
+	water_bar = st.progress(0, text=water_text)
+	
+	with col1.expander("Home"):
+	  st.write("this is a test")
+	
+	with col2.expander("Water Log"):
+	  # choice = st.number_input(label = "Adjust water", step = 1)
+	  st.write("this is a test")
+	  
+	with col3.expander("Food Log"):
+	  st.write("this is a test")
+	  
+	with col4.expander("Exercise Log"):
+	  st.write("this is a test")
+	
+	# water_checkbox = col5.checkbox("Water check")
+	  
+	water_expander = col5.expander("Water log")
+	if (water_expander):
+	    choice = st.number_input(label = "Adjust water", min_value = 0, step = 1, on_change = update)
+	    st.write("this is a test")
+	
+	# USER INPUT + VARIABLE STORING
+	# weight = st.text_input("Enter some text")
+	form = st.form("Water")
+	with st.sidebar:
+	with col5.expander(“Adjust Water”):
+		st.text("BOO")
+	
+	st.image("cup.png")
+	return 0
