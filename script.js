782064ef1e4bed7d7fa57d397c0f641bee836446
Sharon <68297755+sharbearl@users.noreply.github.com>
2024-01-27 08:26:59 -0800

    Update script.js
    Added initial code for functions

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+let cups = 0;
 
+
+function increaseWater()
+{
+ cups = cups + 1; 
+}
+
+function toggleShow()
+{
+  let water_div = document.getElementById("water_dropdown");
+  if (water_div.style.visibility === "visible")
+  {
+    water_div.style.display = "hidden";
+  }
+  else
+  {
+    water_div.style.display = "visible";
+  }
+}
+
+function updateText()
+{
+  let text_div = dovument.getElementById("water_record");
+  text_div.innerHTML = cups;
+}
