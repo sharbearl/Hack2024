811fc21dd87f43ac60e252287e35f955cf85ff23
sunadrian <120338538+sunadrian@users.noreply.github.com>
2024-01-27 10:09:42 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,15 @@
 import streamlit as st
 
+home = st.expander(“Home”)
+water = st.expander(“Water Log”)
+exercise = st.expander(“Exercise Log”)
+food = st.expander(“Food Log”)
+
 # USER INPUT + VARIABLE STORING
-try:
-  weight = st.text_input("Enter some text")
-except ValueError as e:
-  st.text(f"Error: {e}")
+weight = st.text_input("Enter some text")
+while (type(weight) != double or type(weight) != int)
+{
+  st.text("Invalid input")
+}
+
 
