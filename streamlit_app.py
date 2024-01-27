1a0aaa53b20e9e07baaacc46b83bbf61cb80c4e1
sunadrian <120338538+sunadrian@users.noreply.github.com>
2024-01-27 10:50:26 -0800

    testing a checkbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,12 @@ col1, col2, col3, col4 = st.columns(4)
 with col1.expander("Home"):
   st.write("this is a test")
 
-with col2.expander("Water Log"):
+# with col2.expander("Water Log"):
   # choice = st.number_input(label = "Adjust water", step = 1)
-  st.write("this is a test")
+  # st.write("this is a test")
+with col2.checkbox("Water log"):
+    choice = st.number_input(label = "Adjust water", step = 1)
+    st.write("this is a test")
   
 with col3.expander("Food Log"):
   st.write("this is a test")
