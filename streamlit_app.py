7b0241c1370d1d45c525b5be1a6b97563f44469d
Sharon <68297755+sharbearl@users.noreply.github.com>
2024-01-27 10:12:34 -0800

    Update streamlit_app.py
    Added expander text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 home = st.expander("Home")
 water = st.expander("Water Log")
+water.write("this is a test")
 exercise = st.expander("Exercise Log")
 food = st.expander("Food Log")
 
