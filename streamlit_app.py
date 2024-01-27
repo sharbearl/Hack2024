ed1361961a0270b1c2baeeae22cf00a1b135df51
sunadrian <120338538+sunadrian@users.noreply.github.com>
2024-01-27 10:10:15 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-home = st.expander(“Home”)
-water = st.expander(“Water Log”)
-exercise = st.expander(“Exercise Log”)
-food = st.expander(“Food Log”)
+home = st.expander("Home")
+water = st.expander("Water Log")
+exercise = st.expander("Exercise Log")
+food = st.expander("Food Log")
 
 # USER INPUT + VARIABLE STORING
 weight = st.text_input("Enter some text")
