fa9bdda67aa606fab3bffc5f7526a03274d5bc5d
sunadrian <120338538+sunadrian@users.noreply.github.com>
2024-01-27 11:43:36 -0800

    adding everything to a main and experimenting with sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,19 @@
 import streamlit as st
-water_val = 0
+
+def update():
+  water_val += 1
+  update_progress()
+
+def update_progress():
+  water_bar.progress(water_val + 1, text=water_text)
+
+int main():
 
 col1, col2, col3, col4, col5 = st.columns(5)
+water_val = 0;
+
+water_text = "Water"
+water_bar = st.progress(0, text=water_text)
 
 with col1.expander("Home"):
   st.write("this is a test")
@@ -17,24 +29,17 @@ with col4.expander("Exercise Log"):
   st.write("this is a test")
 
 # water_checkbox = col5.checkbox("Water check")
-
-def update():
-  water_val += 1
-  update_progress()
   
 water_expander = col5.expander("Water log")
-print("TEST PRINT")
 if (water_expander):
     choice = st.number_input(label = "Adjust water", min_value = 0, step = 1, on_change = update)
     st.write("this is a test")
 
 # USER INPUT + VARIABLE STORING
 # weight = st.text_input("Enter some text")
+form = st.form("Water")
+with st.sidebar:
+with col5.expander(“Adjust Water”):
+	st.text("BOO")
 
-# choice = st.number_input(label = "Adjust water", step = 1)
 st.image("cup.png")
-
-water_text = "Water"
-water_bar = st.progress(0, text=water_text)
-def update_progress():
-  water_bar.progress(water_val + 1, text=water_text)
