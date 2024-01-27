da2eb74cb62e2911ea940b906a775aaa8192f8a3
sunadrian <120338538+sunadrian@users.noreply.github.com>
2024-01-27 10:20:03 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,5 @@ food = st.expander("Food Log")
 
 # USER INPUT + VARIABLE STORING
 weight = st.text_input("Enter some text")
-# while (type(weight) != double or type(weight) != int)
-# {
-#  st.text("Invalid input")
-# }
 
-
+choice = st.number_input("Adjust water)
